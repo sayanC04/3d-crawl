21aa56b54d87b37144c429db4029aa60418c3e97
Sayan Chakraborty <146632297+sayanC04@users.noreply.github.com>
2026-09-05 23:59:20 +0530

    feat: add SPA support and export app instance
    Configure catch-all route to serve index.html for client-side routing and export the app instance for testability.

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ const PORT = 3000;
 
 app.use(express.static(__dirname));
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
+
+export default app;
